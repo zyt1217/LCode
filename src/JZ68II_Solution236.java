651067ce06c7bc402d6050b05dfe93e058ea94fb
zyt <zyt_1217@outlook.com>
2021-07-30 16:15:41 +0800

    Default Changelist

---
+++ src/JZ68II_Solution236.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class JZ68II_Solution236 {
+    //在类中不要将变量初始化，否则提交会有问题
+    //尽量不适用哈希set来输出，输出不保证有序，也不保证按照插入顺序
+    static List<TreeNode> plist; //= new HashSet<>();
+    static List<TreeNode> qlist; //= new HashSet<>();
+
+    static TreeNode p;
+    static TreeNode q;
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(-1);
+        root.left = new TreeNode(0);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(-2);
+        root.left.right = new TreeNode(4);
+        root.left.left.left = new TreeNode(8);
+        System.out.print(lowestCommonAncestor(root,root.left.left.left,root.left).val);
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == p || root == q || root == null)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if(left != null && right != null)
+            return root;
+        if(left == null && right != null)
+            return right;
+        if(left != null && right == null)
+            return left;
+        return null;
+    }
+
+    public static TreeNode lowestCommonAncestor_(TreeNode root, TreeNode p, TreeNode q) {
+//        p = this.p;
+//        q = this.q;
+        plist = new ArrayList<>();
+        qlist = new ArrayList<>();
+        allAncestor_p(root,p);
+        allAncestor_q(root,q);
+        return qlist.get(0);
+    }
+
+    public static TreeNode allAncestor_p(TreeNode root, TreeNode target){
+        if(root == target){
+            plist.add(root);
+            return root;
+        }else if(root == null)
+            return null;
+        //boolean x = allAncestor_p(root.right, target) || allAncestor_p(root.left, target);
+        if(allAncestor_p(root.right, target) != null || allAncestor_p(root.left, target) != null) {
+            plist.add(root);
+            return root;
+        }else
+            return null;
+    }
+
+    public static TreeNode allAncestor_q(TreeNode root, TreeNode target){
+        if(root == target){
+            if (plist.contains(root)){
+                qlist.add(root);
+            }
+            return root;
+        }else if(root == null)
+            return null;
+        //boolean x = allAncestor_q(root.right, target) || allAncestor_q(root.left, target);
+        if(allAncestor_q(root.right, target) != null || allAncestor_q(root.left, target)!= null) {
+            if (plist.contains(root)){
+                qlist.add(root);
+            }
+            return root;
+        }else
+            return null;
+    }
+}
---
+++ src/JZ68I_Solution235.java
@@ -0,0 +1,106 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class JZ68I_Solution235 {
+    static HashSet<TreeNode> pset = new HashSet<>();
+    static HashSet<TreeNode> qset = new HashSet<>();
+
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+        root.left.right.left = new TreeNode(3);
+        root.left.right.right = new TreeNode(5);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+        lowestCommonAncestor(root,root.left,root.right);
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //迭代
+        if(p.val>q.val){
+            TreeNode temp = p;
+            p = q;
+            q = temp;
+        }
+        while (root!=null){
+            if(root.val>q.val)
+                root = root.left;
+            else if(root.val<p.val)// 此处的左或者右应该是平行关系， 一次循环只能做一次
+                root = root.right;
+            else break;
+        }
+        return root;
+    }
+
+    public static TreeNode lowestCommonAncestor_(TreeNode root, TreeNode p, TreeNode q) {
+        //递归
+        if(p.val>q.val) {
+            TreeNode temp = p;
+            p = q;
+            q = temp;
+        }
+        return dfs(root,p,q);
+    }
+    public static TreeNode dfs(TreeNode root, TreeNode p, TreeNode q){
+        if(root == null)
+            return null;
+        if(root.val<p.val)
+            return dfs(root.right, p, q);
+        if(root.val>q.val)
+            return dfs(root.left,p,q);
+        return root;
+    }
+
+
+
+    public static TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
+        allAncestor_p(root,p);
+        allAncestor_q(root,q);
+        int i = 0;
+        List<TreeNode> list = new ArrayList<>();
+        for(TreeNode t : qset){
+            if((t.val <= Math.max(p.val,q.val)) && (t.val >= Math.min(p.val,q.val))) {
+                list.add(t);
+            }
+        }
+        if(list.size() == 1)
+            return list.get(0);
+        else return (p.left == q || p.right == q) ? p: q;
+    }
+
+    public static boolean allAncestor_p(TreeNode root, TreeNode target){
+        if(root == target){
+            pset.add(root);
+            return true;
+        }else if(root == null)
+            return false;
+        boolean x = allAncestor_p(root.right, target) || allAncestor_p(root.left, target);
+        if(x) {
+            pset.add(root);
+            return true;
+        }else
+            return false;
+    }
+
+    public static boolean allAncestor_q(TreeNode root, TreeNode target){
+        if(root == target){
+            if (pset.contains(root)){
+                qset.add(root);
+            }
+            return true;
+        }else if(root == null)
+            return false;
+        boolean x = allAncestor_q(root.right, target) || allAncestor_q(root.left, target);
+        if(x) {
+            if (pset.contains(root)){
+                qset.add(root);
+            }
+            return true;
+        }else
+            return false;
+    }
+}
