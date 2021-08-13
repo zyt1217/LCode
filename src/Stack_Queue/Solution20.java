c9c10af0bbe676e43387ad2452b3de29bf8eedea
zyt <zyt_1217@outlook.com>
2021-08-13 20:21:37 +0800

    Default Changelist

--- src/Stack_Queue/Solution20.java
+++ src/Stack_Queue/Solution20.java
@@ -1,7 +1,30 @@
 package Stack_Queue;
 
+import java.util.Stack;
+
 public class Solution20 {
     public boolean isValid(String s) {
-
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if (c - '(' == 0 || c - '[' == 0 || c - '{' == 0)
+                stack.push(c);
+            if( c - ')' == 0)
+                if(!stack.isEmpty() && stack.peek() - '(' == 0) {
+                    stack.pop();
+                    continue;
+                }
+                else return false;
+            if(c - ']' == 0)
+                if(!stack.isEmpty() && stack.peek() - '[' == 0) {
+                    stack.pop();
+                    continue;
+                }
+                else return false;
+            if(c - '}' == 0)
+                if (!stack.isEmpty() && stack.peek() - '{' == 0)
+                    stack.pop();
+                else return false;
+        }
+        return stack.isEmpty();
     }
 }
